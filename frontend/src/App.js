46d1c27caea8ca2a7deedf2bab205a5de19d9352
rafiGaucho <rafigaucho10r@gmail.com>
2019-05-16 22:48:57 +0530

    add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
-import React ,{ Component } from 'react';
+import React ,{ Component, Fragment } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import AuthPage from './pages/auth'
 import EventsPage from './pages/events'
 import BookingsPage from './pages/bookings'
+import MainNavigation from './components/Navigation/mainNavigation'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from='/' to='/auth' exact />
-          <Route path='/auth' component={AuthPage}/>
-          <Route path='/events' component={EventsPage}/>
-          <Route path='/bookings' component={BookingsPage}/>
-        </Switch>
+        <Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from='/' to='/auth' exact />
+              <Route path='/auth' component={AuthPage}/>
+              <Route path='/events' component={EventsPage}/>
+              <Route path='/bookings' component={BookingsPage}/>
+            </Switch>
+          </main>
+        </Fragment>
       </BrowserRouter>
     );
   }
---
+++ frontend/src/components/Navigation/mainNavigation.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
+import './mainNavigation.css'
+
+const mainNavigation = props => {
+  return (
+    <header className="main-navigation">
+      <div className="main-navigation__logo">
+        <h1>Event Booker</h1>
+      </div>
+      <nav className="main-navigation__items">
+        <ul>
+          <li>
+            <NavLink to="/auth">Authenticate</NavLink>
+          </li>
+          <li>
+            <NavLink to="/events">Events</NavLink>
+          </li>
+          <li>
+            <NavLink to="/bookings">Bookings</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default mainNavigation
